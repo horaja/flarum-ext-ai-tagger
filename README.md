8ad541f69b9a92fc40d5dee2ec80249e4d61629e
horaja <horaja@andrew.cmu.edu>
2025-06-05 18:19:37 -0400

    Initial commit for ai tagger extension to flarum

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flarum AI Tagger
+
+Author: 
+Husain Raja
+Carnegie Mellon University
+
+AI-powered content tagging for Flarum, a forum-based website.
---
+++ extend.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of horaja/flarum-ext-ai-tagger.
+ *
+ * Author: Husain Raja
+ * Email: horaja@cs.cmu.edu
+ */
+
+namespace Horaja\AiTagger;
+
+use Flarum\Extend;
+
+return [
+    // Backend extenders can go here
+];
