48ba3a2694cdd348bb3284476e713bd2e703d94a
horaja <horaja@andrew.cmu.edu>
2025-06-18 10:24:18 -0400

    update to extend and add a new console command

--- extend.php
+++ extend.php
@@ -11,8 +11,13 @@ namespace Horaja\AiTagger;
 
 use Flarum\Discussion\Event\Saving;
 use Flarum\Extend;
+use Horaja\AiTagger\Console\WorkQueueCommand;
+use Horaja\AiTagger\Listener\AutoTagDiscussion;
 
 return [
-    (new Extend\Event)
-			->listen(Saving::class, Listener\AutoTagDiscussion::class)
+    (new Extend\Event())
+			->listen(Saving::class, AutoTagDiscussion::class),
+
+		(new Extend\Console())
+			->command(WorkQueueCommand::class),
 ];
