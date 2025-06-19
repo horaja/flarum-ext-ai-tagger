d8eb7bf8b8a37235cb1604d6d3ea6b16940f26bf
horaja <horaja@andrew.cmu.edu>
2025-06-19 12:48:17 -0400

    switched to queue extension - fixed queue driver config problem

--- extend.php
+++ extend.php
@@ -11,13 +11,9 @@ namespace Horaja\AiTagger;
 
 use Flarum\Discussion\Event\Saving;
 use Flarum\Extend;
-use Horaja\AiTagger\Console\WorkQueueCommand;
 use Horaja\AiTagger\Listener\AutoTagDiscussion;
 
 return [
     (new Extend\Event())
 			->listen(Saving::class, AutoTagDiscussion::class),
-
-		(new Extend\Console())
-			->command(WorkQueueCommand::class),
 ];
